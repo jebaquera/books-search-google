9c75f655e18f1e426074d14e405404296ad38ae7
jebaquera <jebaquera@gmail.com>
2019-08-01 21:39:42 -0500

    worked on react app files.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Searches the NYT books according to the search criteria given in the form
+  getNewBooks: function(title) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + title);
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets saved Books
+  getSavedBooks: function() {
+    return axios.get("/api/books/");
   },
-  // Deletes the book with the given id
+  // Deletes the Book with the given id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves a book to the database
+  // Saves a Book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
   }
